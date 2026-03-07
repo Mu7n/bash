b25729f357ccd2e3e3ea5bb74bccf866681f1215
Mu <bulzmu98@gmail.com>
2026-03-07 13:57:48 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -ue
+set -u
 red(){ echo -e "\e[31m$1\e[0m";}
 blue(){ echo -e "\e[34m$1\e[0m";}
 purple(){ echo -e "\e[35m$1\e[0m";}
 cyan(){ echo -e "\e[36m$1\e[0m";}
-readp(){ read -p "$(cyan "$1\n")" $2;}
+readp(){ read -p "$(cyan "$1")" $2;}
 
 case $(uname -m) in
   x86_64)     arch_sh="amd64";;
@@ -15,14 +15,15 @@ esac
 
 name_sh="frps"
 link_sh="https://github.com/fatedier/frp/releases/download"
-api_sh="https://api.github.com/repos/fatedier/frp/releases/latest"
-tag_sh="$(curl -s $api_sh | grep 'tag_name' | awk -F '"' '{print $4}' | cut -c 2-)"
+api_sh="$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest)"
+tag_sh="$(echo "$api_sh" | grep '"tag_name"' | awk -F '"' '{print $4}' | cut -c 2-)"
 file_sh="frp_${tag_sh}_linux_${arch_sh}.tar.gz"
 url_sh="${link_sh}/v${tag_sh}/${file_sh}"
-path_sh="/etc/allinone/${name_sh}"
+path_sh="/etc/aio/${name_sh}"
 grep_sh="$(ps -ef | grep $name_sh | grep -v grep | awk '{print $8}')"
+domain_sh="$(ls -l /etc/letsencrypt/live | awk '/^d/ {print $NF}')"
 
-sh_config(){
+sh_configfrp(){
   cat > ${path_sh}/config.toml << TOML
 bindAddr = "0.0.0.0"
 bindPort = 60443
@@ -33,13 +34,11 @@ vhostHTTPSPort = 60443
 auth.method = "token"
 auth.token = "$token_sh"
 
-webServer.addr = "0.0.0.0"
-webServer.port = 60080
-webServer.user = "$USERNAME"
-webServer.password = "$PASSWORD"
-#webServer.tls.certFile = "/etc/letsencrypt/live/\${domain_sh}/fullchain.pem"
-#webServer.tls.keyFile = "/etc/letsencrypt/live/\${domain_sh}/privkey.pem"
-#subDomainHost = "\$domain_sh"
+#webServer.addr = "0.0.0.0"
+#webServer.port = 60080
+#webServer.user = "$USERNAME"
+#webServer.password = "$PASSWORD"
+#subDomainHost = "$domain_sh"
 
 transport.maxPoolCount = 10
 transport.tcpKeepalive = 7200
@@ -59,6 +58,12 @@ log.maxDays = 3
 TOML
 }
 
+sh_file(){
+  mkdir -p -m 644 $path_sh
+  rm -rf ${path_sh}/${name_sh}
+  while true; do if [ -s ${path_sh}/${name_sh} ]; then rm -rf ${file_sh} frp_${tag_sh}_linux_${arch_sh}; ln -sf ${path_sh}/${name_sh} /usr/local/bin; break; else blue "$url_sh，正在下载。"; curl -OL $url_sh && tar xzvf $file_sh && mv -f frp_${tag_sh}_linux_${arch_sh}/${name_sh} ${path_sh} || sleep 5; tag_sh=""; tag_sh="$(echo "$api_sh" | grep '"tag_name"' | awk -F '"' '{print $4}')"; url_sh="${link_sh}/v${tag_sh}/${file_sh}"; fi; done
+}
+
 sh_service(){
   cat > /etc/systemd/system/${name_sh}.service << FRP
 [Unit]
@@ -75,10 +80,8 @@ ExecStart=${path_sh}/${name_sh} -c ${path_sh}/config.toml
 [Install]
 WantedBy=multi-user.target
 FRP
-  chmod 644 /etc/systemd/system/${name_sh}.service
-  systemctl daemon-reload
-  systemctl start $name_sh
-  systemctl enable $name_sh
+  if [ ! -z $grep_sh ]; then pkill -9 $name_sh; fi
+  chmod 644 /etc/systemd/system/${name_sh}.service; systemctl daemon-reload; systemctl start $name_sh; systemctl enable $name_sh
 }
 
 read_token(){
@@ -86,35 +89,17 @@ read_token(){
   readp "请输入password：" PASSWORD
   token_sh="${USERNAME}${PASSWORD}"
   purple "token：$token_sh"
-  while true; do readp "请确认令牌[Yes/No]：" input_sh; case $input_sh in [yY][eE][sS]|[yY]) purple "已确认。"; break;; [nN][oO]|[nN]) blue "请重新输入。"; readp "请输入username：" USERNAME; readp "请输入password：" PASSWORD; token_sh="${USERNAME}${PASSWORD}"; purple "token：$token_sh";; *) red "错误，请重新输入！"; continue;; esac done
+  while true; do readp "请确认令牌[Yes/No]：" input_sh; case $input_sh in [yY][eE][sS]|[yY]) purple "已确认。"; break;; [nN][oO]|[nN]) readp "请输入username：" USERNAME; readp "请输入password：" PASSWORD; token_sh="${USERNAME}${PASSWORD}"; purple "token：$token_sh";; *) red "请重新输入！"; continue;; esac done
 }
 
-sh_file(){
-  blue "下载$file_sh"
-  curl -OL $url_sh
-  blue "提取$file_sh"
-  mkdir -p -m 644 $path_sh
-  tar xzvf $file_sh
-  if [ ! -z $grep_sh ]; then pkill -9 $name_sh; fi
-  mv -f frp_${tag_sh}_linux_${arch_sh}/${name_sh} ${path_sh}
-  rm -rf ${file_sh} frp_${tag_sh}_linux_${arch_sh}
-}
-
-sshd_config(){
-  if [ ! -s /etc/ssh/sshd_config.d/AIO.conf ]; then
-	readp "请输入SSH端口：" sshport_sh
-	purple "SSH端口：$sshport_sh"
-	cat > /etc/ssh/sshd_config.d/AIO.conf << SSHD
-Port $sshport_sh
-PermitRootLogin yes
-PubkeyAuthentication yes
-PasswordAuthentication no
-SSHD
-	ufw allow $sshport_sh
-	ufw allow 60080
-	ufw allow 60443
-	echo "y" | ufw enable
-	systemctl restart sshd
+sh_sshd(){
+  if [ ! -s /etc/ssh/sshd_config.d/sshd.conf ]; then
+    readp "请输入SSH端口：" sshd_sh
+    purple "SSH端口：$sshd_sh"
+    echo -e "PermitRootLogin yes\nPubkeyAuthentication yes\nPasswordAuthentication no\nPort $sshd_sh" > /etc/ssh/sshd_config.d/sshd.conf
+    systemctl restart ssh
+    if [ -s /usr/lib/systemd/system/ssh.socket ]; then sed -i "s/22/$sshd_sh/g" /usr/lib/systemd/system/ssh.socket && systemctl daemon-reload && systemctl restart ssh.socket; fi
+    ufw allow $sshd_sh; ufw allow 60443/tcp; ufw allow 60443/udp; echo "y" | ufw enable >/dev/null
   fi
 }
 
@@ -126,16 +111,10 @@ if [ -s ${path_sh}/${name_sh} ]; then
 	blue "1、升级"
 	blue "2、退出"
 	readp "请输入选项：" option_sh
-	case $option_sh in 1) if [ ! -z $tag_sh ]; then sh_file; read_token; sh_config; sh_service; sshd_config; fi; break;; 2) blue "退出。"; break;; *) red "错误，请重新输入！"; continue;; esac
+	case $option_sh in 1) sh_file; systemctl restart $name_sh break;; 2) break;; *) red "请重新输入！"; continue;; esac
   done
-else
-  sh_file
-  read_token
-  sh_config
-  sh_service
-  sshd_config
 fi
 
-ufw status
-service $name_sh status
+ufw status; export SYSTEMD_PAGER=""; service $name_sh status
+
 purple "\nEND！"
