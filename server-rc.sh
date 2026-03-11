8fc5282d10e3ec9e5e0680128d18238bfeb47954
Mu <bulzmu98@gmail.com>
2026-03-11 16:46:48 +0800

    Update server-rc.sh

--- server-rc.sh
+++ server-rc.sh
@@ -91,6 +91,10 @@ server {
     server_name $domain_sh;
     ssl_certificate /etc/letsencrypt/live/${domain_sh}/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/${domain_sh}/privkey.pem;
+    location ~ ^/sub/(mihomo|xray|file)/(.*) {
+        default_type 'text/plain; charset=utf-8';
+        alias /etc/aio/subscribe/$1/$2;
+    }
     location /${public_sh} { #与 reality-xhttp 中 path 对应
         grpc_pass grpc://127.0.0.1:44308; #转发 reality-xhttp 监听进程
         grpc_set_header Host \$host;
