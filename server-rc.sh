5c2c83cabfc1ba86a611c9884cb5d340b9130ecb
Mu <bulzmu98@gmail.com>
2026-03-16 16:44:36 +0800

    Update server-rc.sh

--- server-rc.sh
+++ server-rc.sh
@@ -9,7 +9,7 @@ elif { [ -f "/etc/issue" ] && grep -qi "debian" /etc/issue; } || { [ -f "/etc/os
 elif { [ -f "/etc/issue" ] && grep -qi "Ubuntu" /etc/issue; } || { [ -f "/etc/os-release" ] && grep -qi "ID=ubuntu" /etc/os-release; }; then
   release="ubuntu"; service="service"; nginxpid="/run/nginx.pid"; nginxconf="/etc/nginx/sites-enabled/default"; if ! type "nginx" "certbot" "unzip" "tar" "qrencode" "ufw" >/dev/null 2>&1; then blue "开始安装。"; apt-get update -y && apt install -y nginx certbot python3-certbot-nginx unzip tar qrencode ufw; fi
 fi
-if [[ -z "$release" ]]; then red "未知系统！"; exit 0; else case "$(uname -m)" in amd64 | x86_64) serverarch="64";; armv8 | aarch64) serverarch="arm64-v8a";; i386 | i686) serverarch="32";; *) red "未知架构！"; exit 0;; esac fi
+if [[ ! -z "$release" ]]; then case "$(uname -m)" in amd64 | x86_64) serverfile="Xray-linux-64.zip";; armv8 | aarch64) serverfile="Xray-linux-arm64-v8a.zip";; i386 | i686) serverfile="Xray-linux-32.zip";; *) red "未知架构！"; exit 0;; esac; else red "未知系统！"; exit 0; fi
 
 red(){  echo -e "\e[31m$1\e[0m";}
 blue(){  echo -e "\e[34m$1\e[0m";}
@@ -21,7 +21,6 @@ servername="xray"
 serversite="https://github.com/XTLS/Xray-core/releases/download"
 serverapi="https://api.github.com/repos/XTLS/Xray-core/releases/latest"
 servertag="$(curl -sf "$serverapi" | grep '"tag_name"' | awk -F '"' '{print $4}')"
-serverfile="Xray-linux-${serverarch}.zip"
 serverurl="${serversite}/${servertag}/${serverfile}"
 serverpath="/etc/aio/${servername}"
 subscribepath="/etc/aio/subscribe"
