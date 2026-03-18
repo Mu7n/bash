8908a6c044703aeefc37180f66cfc540adddd703
Mu <bulzmu98@gmail.com>
2026-03-18 12:52:21 +0800

    Update server-rc.sh

--- server-rc.sh
+++ server-rc.sh
@@ -408,10 +408,10 @@ DOMAIN(){
 
 CERT(){
   blue "申请SSL证书。"
-  echo -e "#!/usr/bin/env bash\nservice nginx restart" > /etc/letsencrypt/renewal-hooks/deploy/renewcert.sh && chmod +x /etc/letsencrypt/renewal-hooks/deploy/renewcert.sh
+  echo -e "#!/usr/bin/env bash\nservice nginx restart" > /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh && chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+  echo -e "0 0 1 * * certbot renew --renew-hook 'service nginx restart'" > /var/spool/cron/crontabs/root
   echo -e "server {\n    listen 80;\n    listen [::]:80;\n    server_name $serverdomain;\n}" > $nginxconf
   service nginx restart && certbot --nginx --force-renewal --agree-tos -n -m ssl@cert.bot -d $serverdomain
-  echo -e "0 0 1 * * certbot renew --renew-hook 'service nginx restart'" > /var/spool/cron/crontabs/root
 }
 
 SSHD(){
