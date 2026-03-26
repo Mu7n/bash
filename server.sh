eb1a28a706bd927338700d01b7d370feb3eb5ac3
Mu <bulzmu98@gmail.com>
2026-03-26 09:26:22 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -368,9 +368,9 @@ SUBSCRIBE(){
   mkdir -p -m 555 ${serversubpath}/xlink
   mkdir -p -m 555 ${serversubpath}/mlink
   cat > ${serversubpath}/xray << XSUB
-vless://$(echo -n ":${xuuid}@${xdomain}:443" | base64 -w 0)?type=tcp&obfs=xtls-rprx-vision&flow=xtls-rprx-vision&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&udp=xudp#vision
+vless://$(echo -n ":${xuuid}@${xdomain}:443" | base64 -w 0)?type=tcp&flow=xtls-rprx-vision&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&udp=xudp#vision
 vless://$(echo -n ":${xuuid}@${xdomain}:443" | base64 -w 0)?type=xhttp&obfs=xhttp&path=${xrpk}&mode=auto&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&udp=xudp#xhttp
-vless://$(echo -n ":${xuuid}@${xdomain}:10723" | base64 -w 0)?type=mkcp&obfs=mkcp&obfsParam=%7B%22header%22:%22utp%22,%22congestion%22:%22true%22,%22mtu%22:%22100%22,%22tti%22:%2230%22,%22uplinkCapacity%22:%22100%22,%22downlinkCapacity%22:%22300%22,%22seed%22:%22${xuuid}%22%7D#mkcp
+vless://$(echo -n ":${xuuid}@${xdomain}:10723" | base64 -w 0)?type=mkcp&obfs=mkcp&obfsParam=%7B%22header%22:%22utp%22,%22congestion%22:%22true%22,%22mtu%22:%22100%22,%22tti%22:%2230%22,%22uplinkCapacity%22:%22100%22,%22downlinkCapacity%22:%22300%22,%22seed%22:%22${xuuid}%22%7D&udp=xudp#mkcp
 
 XSUB
   cat > ${serversubpath}/mihomo << MSUB
@@ -381,17 +381,16 @@ proxies:
     port: 443
     uuid: $xuuid
     network: tcp
-    obfs: xtls-rprx-vision
     flow: xtls-rprx-vision
     reality-opts:
       public-key: $xrpk
       short-id: $xsid
-    sni: $xdomain
     tls: true
-    udp: true
-    packet-encoding: xudp
+    sni: $xdomain
     skip-cert-verify: false
     client-fingerprint: chrome
+    udp: true
+    packet-encoding: xudp
   - name: "xhttp"
     type: vless
     server: $xdomain
@@ -403,12 +402,12 @@ proxies:
     reality-opts:
       public-key: $xrpk
       short-id: $xsid
-    sni: $xdomain
     tls: true
-    udp: true
-    packet-encoding: xudp
+    sni: $xdomain
     skip-cert-verify: false
     client-fingerprint: chrome
+    udp: true
+    packet-encoding: xudp
   - name: "mkcp"
     type: vless
     server: $xdomain
@@ -416,13 +415,13 @@ proxies:
     uuid: $xuuid
     network: mkcp
     obfs: mkcp
-    obfs-header: utp
-    obfs-congestion: true
-    obfs-mtu: 100
-    obfs-tti: 30
-    obfs-up: "100 Mbps"
-    obfs-down: "300 Mbps"
-    obfs-password: $xuuid
+      header: utp
+      congestion: true
+    mtu: "100"
+    tti: "30"
+    up: "100 Mbps"
+    down: "300 Mbps"
+    password: $xuuid
     udp: true
     packet-encoding: xudp
 
