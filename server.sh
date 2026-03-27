b5e2ef8d6fab0ba4569377b0c76fab1d7f998a03
Mu <bulzmu98@gmail.com>
2026-03-27 12:32:29 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -249,7 +249,7 @@ REALITY(){
         "network": "kcp",
         "mtu": 1350,
         "tti": 50,
-        "uplinkCapacity": 50,
+        "uplinkCapacity": 100,
         "downlinkCapacity": 100,
         "congestion": true,
         "readBufferSize": 5,
@@ -374,7 +374,7 @@ SUBSCRIBE(){
   cat > ${serversubpath}/xray << XSUB
 vless://${xuuid}@${xdomain}:443?type=tcp&tls=true&flow=xtls-rprx-vision&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&xudp=true#vision
 vless://$(echo -n ":${xuuid}@${xdomain}:443" | base64 -w 0)?type=xhttp&obfs=xhttp&path=${xrpk}&mode=auto&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&udp=xudp#xhttp
-vless://$(echo -n ":${xuuid}@${xdomain}:10723" | base64 -w 0)?type=mkcp&obfs=mkcp&obfsParam=%7B%22header%22:%22srtp%22,%22congestion%22:%22true%22,%22mtu%22:%221350%22,%22tti%22:%2250%22,%22uplinkCapacity%22:%2220%22,%22downlinkCapacity%22:%2250%22,%22seed%22:%22${xuuid}%22%7D&mux=true&xudp=true#mkcp
+vless://$(echo -n ":${xuuid}@${xdomain}:10723" | base64 -w 0)?type=mkcp&obfs=mkcp&obfsParam=%7B%22header%22:%22srtp%22,%22congestion%22:%22true%22,%22mtu%22:%221350%22,%22tti%22:%2250%22,%22uplinkCapacity%22:%2225%22,%22downlinkCapacity%22:%22100%22,%22seed%22:%22${xuuid}%22%7D&mux=true&xudp=true#mkcp
 
 XSUB
   cat > ${serversubpath}/mihomo << MSUB
