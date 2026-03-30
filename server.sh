94ba3e71e569a01ecf4672c9a636ff4905c20184
Mu <bulzmu98@gmail.com>
2026-03-30 12:12:31 +0800

    Refactor vless and hysteria2 URLs in server.sh

--- server.sh
+++ server.sh
@@ -424,10 +424,10 @@ SUBSCRIBE(){
   mkdir -p -m 555 ${serversubpath}/xlink
   mkdir -p -m 555 ${serversubpath}/mlink
   cat > ${serversubpath}/xray << XSUB
-vless://${xuuid}@${xdomain}:443?type=tcp&tls=true&flow=xtls-rprx-vision&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&xudp=true#vision
-vless://$(echo -n ":${xuuid}@${xdomain}:443" | base64 -w 0)?type=xhttp&obfs=xhttp&path=${xrpk}&mode=auto&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome&udp=xudp#xhttp
+vless://${xuuid}@${xdomain}:443?type=tcp&flow=xtls-rprx-vision&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome#vision
+vless://${xuuid}@${xdomain}:443?type=xhttp&path=${xrpk}&mode=auto&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome#xhttp
 vless://$(echo -n ":${xuuid}@${xdomain}:10723" | base64 -w 0)?type=mkcp&obfs=mkcp&obfsParam=%7B%22header%22:%22dtls%22,%22congestion%22:%22true%22,%22uplinkCapacity%22:%2215%22,%22downlinkCapacity%22:%22125%22,%22seed%22:%22${xsid}%22%7D&mux=true&xudp=true#mkcp
-hysteria2://${xuuid}@${xdomain}:23710?sni=${xdomain}&keepalive=30&upmbps=100&downmbps&=200&alpn=h3&insecure=0#hy2
+hysteria2://${xuuid}@${xdomain}:23710?sni=${xdomain}&keepalive=30&upmbps=100&downmbps&=200&alpn=h3#hy2
 XSUB
   cat > ${serversubpath}/mihomo << MSUB
 proxies:
@@ -453,8 +453,9 @@ proxies:
     port: 443
     uuid: $xuuid
     network: xhttp
-    obfs: xhttp
-    path: $xrpk
+    xhttp-opts:
+      path: $xrpk
+      mode: auto
     reality-opts:
       public-key: $xrpk
       short-id: $xsid
