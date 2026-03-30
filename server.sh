cb281d7c9e99f2c479bf6f93db3ee98222b901c2
Mu <bulzmu98@gmail.com>
2026-03-30 12:29:57 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -422,7 +422,7 @@ SUBSCRIBE(){
   mkdir -p -m 555 ${serversubpath}/mlink
   cat > ${serversubpath}/xray << XSUB
 vless://${xuuid}@${xdomain}:443?type=tcp&flow=xtls-rprx-vision&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome#vision
-vless://${xuuid}@${xdomain}:443?type=xhttp&path=${xrpk}&mode=auto&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome#xhttp
+vless://${xuuid}@${xdomain}:443?type=xhttp&path=${xuuid}&mode=auto&tls=true&security=reality&sni=${xdomain}&pbk=${xrpk}&sid=${xsid}&fp=chrome#xhttp
 vless://$(echo -n ":${xuuid}@${xdomain}:10723" | base64 -w 0)?type=mkcp&obfs=mkcp&obfsParam=%7B%22header%22:%22dtls%22,%22congestion%22:%22true%22,%22uplinkCapacity%22:%2215%22,%22downlinkCapacity%22:%22125%22,%22seed%22:%22${xsid}%22%7D&mux=true&xudp=true#mkcp
 hysteria2://${xuuid}@${xdomain}:23710?sni=${xdomain}&keepalive=30&upmbps=100&downmbps&=200&alpn=h3#hy2
 XSUB
@@ -451,7 +451,7 @@ proxies:
     uuid: $xuuid
     network: xhttp
     xhttp-opts:
-      path: $xrpk
+      path: $xuuid
       mode: auto
     reality-opts:
       public-key: $xrpk
